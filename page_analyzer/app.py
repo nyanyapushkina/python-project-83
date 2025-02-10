76ca9eb359af8e31e1489d8b866a134f538169b5
arina.khokhlova <arina.khokhlova@KHOKHLOVAAV-MB.local>
2025-02-10 22:08:06 +0300

    Add load_dotenv

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 @app.route('/')
 def home():
-    return "Welcome to the Page Analyzer!"
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
