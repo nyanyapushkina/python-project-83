2f9d49d8342da52b9b39813446f46f5b00a941a0
arina.khokhlova <arina.khokhlova@KHOKHLOVAAV-MB.local>
2025-02-14 17:29:48 +0300

    Add DATABASE

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,3 +1,4 @@
+import psycopg2
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
@@ -7,6 +8,9 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.route('/')
 def home():
