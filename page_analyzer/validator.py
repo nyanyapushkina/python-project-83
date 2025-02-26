5aaad5463ec4d5b84994ba62fb6ec11eaf721048
arina.khokhlova <arina.khokhlova@KHOKHLOVAAV-MB.local>
2025-02-26 13:02:51 +0300

    Update validator

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -6,12 +6,17 @@ from page_analyzer.database import get_urls_by_name
 def validate_url(url):
     if not url:
         return {'url': url, 'error': 'zero'}
+
     if len(url) > 255:
         return {'url': url, 'error': 'length'}
+
     if not validators.url(url):
         return {'url': url, 'error': 'invalid'}
 
     parsed_url = urlparse(url)
+    if not parsed_url.scheme or not parsed_url.netloc:
+        return {'url': url, 'error': 'invalid'}
+
     norm_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
 
     if get_urls_by_name(norm_url):
