6906ac2fffda26fb7b304beab6975bd554af25d5
arina.khokhlova <arina.khokhlova@KHOKHLOVAAV-MB.local>
2025-03-12 12:58:58 +0300

    Minor lint fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,6 +3,7 @@ from page_analyzer.routes.main import main_bp
 from page_analyzer.routes.urls import urls_bp
 from page_analyzer.config import Config
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
